8face7d7a0d6af35be16b56a899d6937141b8262
Elyahu Jacobi <Elyahu41@gmail.com>
2023-02-20 03:36:12 -0500

    Initial Commit

---
+++ Rabbi Ovadiah Yosef Calendar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rabbi Ovadiah Yosef Calendar
+//
+//  Created by Elyahu on 2/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
